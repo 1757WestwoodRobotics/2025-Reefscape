d8701ed1c4cf12624788445c3e095c06fc7dce40
icai0 <icaiprogramming@gmail.com>
2025-01-22 20:45:33 -0500

    pylint??

--- subsystems/visionsubsystem.py
+++ subsystems/visionsubsystem.py
@@ -125,9 +125,10 @@ class VisionSubsystem(Subsystem):
                 tagsUsed = camEstPose.targetsUsed
 
                 isMultitag = len(tagsUsed) > 1
-                # pylint:disable-next=protected-access
+
                 if isMultitag:
                     # definitelynotTODO: fix this
+                    # pylint:disable-next=protected-access
                     avgDistance = (
                         botPose.transformBy(camera.robotToCameraTransform)
                         .relativeTo(estimator._fieldTags.getOrigin())
