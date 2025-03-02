1b3df6cd3eef649b9f7e20c0aec5db06930e6346
icai0 <icaiprogramming@gmail.com>
2025-03-02 14:49:08 -0500

    imports

--- commands/elevatorsetting.py
+++ commands/elevatorsetting.py
@@ -1,6 +1,8 @@
 from commands2 import Command
 from wpilib import Timer
 from subsystems.elevatorsubsystem import ElevatorSubsystem
+from ntcore import NetworkTableInstance
+import constants
 
 
 class SetElevatorState(Command):
@@ -76,3 +78,72 @@ class ElevatorIntakePosition(SetElevatorState):
 
     def execute(self) -> None:
         self.elevator.setIntakePosition()
+
+
+class ElevatorManualMode(SetElevatorState):
+    def __init__(self, elevatorSubsystem: ElevatorSubsystem) -> None:
+        SetElevatorState.__init__(self, elevatorSubsystem)
+
+    def execute(self) -> None:
+        self.elevator.setManualMode()
+
+
+class ElevatorManualUp(Command):
+    def __init__(self, elevator: ElevatorSubsystem) -> None:
+        Command.__init__(self)
+        self.setName(__class__.__name__)
+        self.elevator = elevator
+
+        self.elevatorPositionPublisher = (
+            NetworkTableInstance.getDefault()
+            .getFloatTopic(constants.kElevatorPositionKey)
+            .publish()
+        )
+        self.elevatorPositionGetter = (
+            NetworkTableInstance.getDefault()
+            .getFloatTopic(constants.kElevatorAtPositionKey)
+            .subscribe(constants.kIntakePositionBeltPosition)
+        )
+        self.elevatorStateGetter = (
+            NetworkTableInstance.getDefault()
+            .getStringTopic(constants.kElevatorStateKey)
+            .subscribe("ElevatorState.IntakePosition")
+        )
+
+    def execute(self):
+        print("manualup")
+        if self.elevatorStateGetter.get() == "ElevatorState.ManualMode":
+            self.elevatorPositionPublisher.set(
+                self.elevatorPositionGetter.get() + constants.kElevatorManualIncrement
+            )
+            print("manual upppppppppp")
+
+
+class ElevatorManualDown(Command):
+    def __init__(self, elevator: ElevatorSubsystem) -> None:
+        Command.__init__(self)
+        self.setName(__class__.__name__)
+        self.elevator = elevator
+        self.addRequirements(self.elevator)
+
+        self.elevatorPositionPublisher = (
+            NetworkTableInstance.getDefault()
+            .getFloatTopic(constants.kElevatorPositionKey)
+            .publish()
+        )
+        self.elevatorPositionGetter = (
+            NetworkTableInstance.getDefault()
+            .getFloatTopic(constants.kElevatorAtPositionKey)
+            .subscribe(constants.kIntakePositionBeltPosition)
+        )
+        self.elevatorStateGetter = (
+            NetworkTableInstance.getDefault()
+            .getStringTopic(constants.kElevatorStateKey)
+            .subscribe("ElevatorState.IntakePosition")
+        )
+
+    def execute(self):
+        if self.elevatorStateGetter.get() == "ElevatorState.ManualMode":
+            self.elevatorPositionPublisher.set(
+                self.elevatorPositionGetter.get() - constants.kElevatorManualIncrement
+            )
