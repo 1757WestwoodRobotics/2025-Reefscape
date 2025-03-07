2ec7246670a632c3af570dfcf9c0dd7ab1a3f71c
LandonBayer <landonbayer0@gmail.com>
2025-03-06 19:23:38 -0800

    crimes

--- robotcontainer.py
+++ robotcontainer.py
@@ -197,7 +197,19 @@ class RobotContainer:
         POVButton(*self.operatorInterface.elevatorAlgaeLow).whileTrue(
             ElevatorAlgaeLow(self.elevator).repeatedly()
         )
+        POVButton(*self.operatorInterface.elevatorAlgaeLow2).whileTrue(
+            ElevatorAlgaeLow(self.elevator).repeatedly()
+        )
+        POVButton(*self.operatorInterface.elevatorAlgaeLow3).whileTrue(
+            ElevatorAlgaeLow(self.elevator).repeatedly()
+        )
         POVButton(*self.operatorInterface.elevatorAlgaeHigh).whileTrue(
+            ElevatorAlgaeHigh(self.elevator).repeatedly()
+        )
+        POVButton(*self.operatorInterface.elevatorAlgaeHigh2).whileTrue(
+            ElevatorAlgaeHigh(self.elevator).repeatedly()
+        )
+        POVButton(*self.operatorInterface.elevatorAlgaeHigh3).whileTrue(
             ElevatorAlgaeHigh(self.elevator).repeatedly()
         )
         ModifiableJoystickButton(self.operatorInterface.elevatorL1Toggle).toggleOnTrue(
