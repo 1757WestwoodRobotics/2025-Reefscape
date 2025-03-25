25ba68d357ba37db7ace9d56063a5ba7a0d2a93a
Luke Maxwell <24lbmaxwell@protonmail.com>
2025-03-24 21:48:08 -0400

    pose combination

--- subsystems/vision/visionio.py
+++ subsystems/vision/visionio.py
@@ -1,12 +1,14 @@
 from typing import Optional
-from wpimath.geometry import Pose3d, Rotation2d, Transform3d
+from wpimath.geometry import Rotation2d, Transform3d
+
+from subsystems.drivesubsystem import VisionObservation
 
 
 class VisionSubsystemIO:
     def updateCameraPosition(self, transform: Transform3d) -> None:
         raise NotImplementedError("Must be implemented by subclass")
 
-    def getRobotFieldPose(self) -> Optional[Pose3d]:
+    def getRobotFieldPose(self) -> Optional[VisionObservation]:
         raise NotImplementedError("Must be implemented by subclass")
 
     def updateRobotYaw(self, yaw: Rotation2d) -> None:
