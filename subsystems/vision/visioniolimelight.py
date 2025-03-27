ed13d5027f5860d7c242af4a192921372b1955e5
icai0 <icaiprogramming@gmail.com>
2025-03-27 16:55:52 -0400

    fix transforms

--- subsystems/vision/visioniolimelight.py
+++ subsystems/vision/visioniolimelight.py
@@ -9,8 +9,9 @@ import constants
 
 
 class VisionSubsystemIOLimelight(VisionSubsystemIO):
-    def __init__(self, name: str) -> None:
+    def __init__(self, name: str, transform: Transform3d) -> None:
         VisionSubsystemIO.__init__(self)
+        self.location = transform
         self.cameraTable = NetworkTableInstance.getDefault().getTable(name)
         self.validTarget = self.cameraTable.getIntegerTopic("tv").subscribe(0)
         self.pipelineLatency = self.cameraTable.getIntegerTopic("tl").subscribe(0)
