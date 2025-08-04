e9ee1ce7f55f8f2b5338fa3ba6315d0c1d86601e
3arc7 <szigetym@gmail.com>
2025-08-04 13:42:39 -0400

    Added constants and comments

--- constants.py
+++ constants.py
@@ -936,7 +936,7 @@ kVelocityControlMotorType = DCMotor.falcon500()
 kVelocityControlkV = 0.01
 
 # algae grabber motor constants
-kAlgaeCANID = 20 #placeholder (fix later)
+kAlgaeCANID = 20  # placeholder (fix later)
 kAlgaeName = "AlgaeMotor"
 kAlgaePGain = 0.8
 kAlgaeIGain = 0
@@ -971,10 +971,10 @@ kPivotEncoderID = 46
 kPivotEncoderOffset = 0.23
 
 kIntakeMotorSpeed = 0.25
-kIntakeAlgaeMotorSpeed = 0.75
-kAlgaeMotorScoreSpeed = 0.8
-kScoreAlgaeBargeMotorSpeed = 0.6
-kScoreAlgaeProcessorMotorSpeed = 0.5
+kIntakeAlgaeMotorSpeed = 0.75  # CALIBRATION NEEDED!
+kAlgaeMotorScoreSpeed = 0.8  # CALIBRATION NEEDED!
+kScoreAlgaeBargeMotorSpeed = 0.6  # CALIBRATION NEEDED!
+kScoreAlgaeProcessorMotorSpeed = 0.5  # CALIBRATION NEEDED!
 kIntakeL1MotorSpeed = 0.35
 kIntakeL2ThroughL4MotorSpeed = 0.20
 # CAD angles, taken from horizontal
@@ -984,8 +984,9 @@ kIntakeToArmOffset = 51.944769
 kIntakingAngle = Rotation2d.fromDegrees(257)
 kMaxPivotAngle = Rotation2d.fromDegrees(260)
 kScoreAngle = Rotation2d.fromDegrees(180 - kIntakeToArmOffset - 45.047053)
-kArmClawRemovalAngle = Rotation2d(-17.5)
-kAlgaeScoreAngle = Rotation2d.fromDegrees # () please fill in later
+kArmClawRemovalAngle = Rotation2d(-17.5)  # CALIBRATION NEEDED!
+kArmClawGroundAngle = Rotation2d(17.5)  # CALIBRATION NEEDED!
+kAlgaeScoreAngle = Rotation2d(25)  # CALIBRATION NEEDED!
 
 kIntakeAtPositionKey = "intake/atPosition"
 kPivotAngleKey = "intake/pivotAngle"
@@ -1077,9 +1078,9 @@ kL2PositionBeltPosition = 14.5 * kMetersPerInch
 kL1PositionBeltPosition = 0.5 * kMetersPerInch
 kIntakePositionBeltPosition = 41 * kMetersPerInch
 kIntakePositionCoralSpaceBeltPosition = 38.5 * kMetersPerInch
-kAlgaeRemovalLowBeltPosition = 25 * kMetersPerInch
-kAlgaeRemovalHighBeltPosition = 31.5 * kMetersPerInch
-# kAlgaeIntakeLollipopBeltPosition = please fill later
+kAlgaeRemovalLowBeltPosition = 25 * kMetersPerInch  # CALIBRATION NEEDED!
+kAlgaeRemovalHighBeltPosition = 31.5 * kMetersPerInch  # CALIBRATION NEEDED!
+kAlgaeIntakeLollipopBeltPosition = 5 * kMetersPerInch  # CALIBRATION NEEDED!
 
 kElevatorFudgeAmount = 0.5 * kMetersPerInch
 
